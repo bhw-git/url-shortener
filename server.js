dac0fedd977d2252f7968603dd4f12cf78bb1596
bhw-git <bhuvaneshbhwsekar2002@gmail.com>
2025-10-31 00:04:06 +0530

    Added EJS file & Styling

--- server.js
+++ server.js
@@ -6,51 +6,58 @@ import Url from "./models/Url.js";
 
 dotenv.config();
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 const port = 3000;
 
-mongoose.connect(process.env.MONGO_URI, {})
-    .then(() => console.log("MongoDB connected"))
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+//Database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("✅ MongoDB connected"))
     .catch((err) => console.log(err));
 
+// Home Page
 app.get("/", (req, res) => {
-    res.sendFile(process.cwd() + "/public/index.html");
+    res.render("index.ejs");
 });
 
+// Shorten URL - Handle form submission
 app.post("/shorten", async(req, res) => {
     const {longUrl} = req.body;
-    console.log("longUrl",longUrl);
     const base = "http://localhost:3000";
     const urlCode = shortid.generate();
 
     try {
         let url = await Url.findOne({longUrl});
-        console.log("url",url);
+        let shortUrl = `${base}/${urlCode}`;
+
         if(url) {
-            console.log("urlCode",urlCode);
-            return res.json(url); 
+            // console.log("shortUrl inside if()",shortUrl);
+            return res.render("result.ejs",{shortUrl: url.shortUrl}); 
         }
-        console.log("urlCode",urlCode);
-        const shortUrl = `${base}/${urlCode}`;
+        // console.log("shortUrl outside if()",shortUrl);
         url = new Url({urlCode, longUrl, shortUrl});
         await url.save();
-        res.json(url);
+        res.render("result.ejs",{shortUrl: url.shortUrl});
+
     } catch (error) {
         console.error(error);
         res.status(500).json("Server Error");
     }
 });
 
+//To fetch & redirect shortened link to actual link using urlCode
 app.get("/:code", async(req, res) => {
     try {
         const url = await Url.findOne({urlCode: req.params.code });
         console.log("/:code", url);
         if(url){
-            console.log(url.longUrl);
             return res.redirect(url.longUrl);
         } else {
-            return res.status(404).json("No URL Found");
+            res.status(404).json("URL not found");
         }
     } catch (error) {
         console.error(error);
